13343efc9c27280137f12f640cd98e1c8ce21d24
Farkhatzhan Makhmudov <faralost@gmail.com>
2023-03-17 15:23:43 +0600

    add user model. migrate

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,9 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,27 @@
-from shop import db
+from shop import db, bcrypt
+
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String(50), unique=True, nullable=True)
+    created_on = db.Column(db.DateTime, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, is_admin=False, phone=None):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+        self.phone = phone
+        self.created_on = datetime.now()
+        self.is_admin = is_admin
+
+    def __repr__(self):
+        return f'{self.email}'
 
 
 class Item(db.Model):
